266ac5f0a08e5df5a5042048417af2089a7067be
ashik13en <ashikurrahman13en@gmail.com>
2024-09-07 21:58:07 +0600

    added js

---
+++ first-js/first.js
@@ -0,0 +1,67 @@
+// Javascript is a Case Sensitive
+// This is a single-line comment
+/* This is a 
+   multi-line comment */
+
+
+//    console.log() output dekhar jonno use kore
+
+
+ /*  variables  */
+//  data store kore
+// variables tumar tin type er hoi
+//         1. var
+//         2. let
+//         3. const
+
+    // var: The old way, not recommended for modern JavaScript.
+    // var er majhe tmi dui type er data rakhte paro eccha korle change korte parba abar unchange o korte parba
+
+    // let: The modern way to declare variables that can change
+    // let use korle tmi data  change  korte parba
+
+
+    // const use korle data change kora jai na because const fixed data rakhe
+
+
+    // declear variables
+
+    // var age = 3 ;
+    // let memberName = "jhon";
+    // const thikana = "Bangadesh";
+
+    // console.log( age , memberName,thikana)
+
+
+    // Data types
+    // 5 doroner datatypes ache
+        // 1. string 
+        // 2. number
+        // 3. boolean
+        // 4. null
+        // 5. undefined
+
+        // let myString = "Hello, World!";
+        // let myNumber = 42;
+        // let myBoolean = false;
+        // let myNull = null;
+        // let myUndefined;
+
+        // console.log(myString,myNumber,myBoolean,myNull,myUndefined)
+
+        // Simple Operations with Variables:
+
+        // let a = 50;
+        // let b = 10;
+        // let sum = a + b;  // Sum of two numbers
+
+        // let nam = "Fuad";
+        // let greeting = "Happy birthday" + nam;  // Concatenating strings
+
+        // console.log(greeting)
+
+        // // Home work
+        // 1. what is js?
+        // 2. declare korba variables.
+        // 3. output dekhaiba variables.
+        // 4. ki ki data type ache ar kondar ki kaj eidao leikka aynno
